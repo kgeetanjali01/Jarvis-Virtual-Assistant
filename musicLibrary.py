0873e6670a5d1825f14facfd66a203703385e08a
Geetanjali Kumari <kumarigeetanjali21@gmail.com>
2024-09-22 21:38:24 +0530

    Initial commit

---
+++ musicLibrary.py
@@ -0,0 +1,6 @@
+music = {
+    "stealth" : "https://youtu.be/U47Tr9BB_wE?si=UZHm-pb0vFs_WXv9",
+    "smile" : "https://youtu.be/6wlbB1PTzJU?si=6sSRVMcOSQSE2UMY",
+    "skies" : "https://youtu.be/5kDO8hLu9iI?si=zdT4-t8ht6jA8Idk",
+    "crimson" : "https://youtu.be/qpxhgby-ONI?si=u1F3ohgR88xljH0g"
+}    
